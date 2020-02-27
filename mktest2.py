af6c1aa37d581fe790b66936ebae1054d38c16c6
lxxlccly <975716738@qq.com>
2020-02-27 14:21:00 +0800

    mktest2

---
+++ mktest2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import re
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    document = args[0]
+    query = args[1]
+    with open(query) as query_file:
+        for word in query_file:
+            print(word)
