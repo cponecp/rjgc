d6b11c1c94d09694165dd4bc4f4296caccb2adab
lxxlccly <975716738@qq.com>
2020-02-27 16:55:35 +0800

    mktest2

--- mktest2.py
+++ mktest2.py
@@ -9,12 +9,28 @@ def find_word(word, document):
         for line in document_file:
             all_line.append(line)
     sentence = 0
+    word_amount = 0
     word_punctuation = []
     for i in range(len(all_line)):
         line_list = re.findall('\w+|\.|\?|\!', all_line[i])
         word_punctuation += line_list
-        for j in range(len(word_punctuation)):
-            print(word_punctuation[j], end=' ')
+        while 1:
+            has_punctuation = 0
+            for j in range(len(word_punctuation)):
+                if word_punctuation[j] in {'.', '!', '?'}:
+                    has_punctuation = 1
+                    sentence += 1
+                    for k in range(0, j):
+                        if word_punctuation[k] == word:
+                            word_amount += 1
+                            print('{0}/{1},'.format(sentence, k+1), end='')
+                    for k in range(j, -1, -1):
+                        del word_punctuation[k]
+                    break
+            if has_punctuation == 0:
+                break
+    if word_amount == 0:
+        print('None', end='')
     return 0
 
 
@@ -26,3 +42,4 @@ if __name__ == '__main__':
         for word in query_file:
             word = word.strip("\n")
             find_word(word, document)
+            print()
