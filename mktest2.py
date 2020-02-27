f5391cac16de40f486ecb3324b91b2ae2c8d3fe9
lxxlccly <975716738@qq.com>
2020-02-27 16:02:35 +0800

    mktest2

--- mktest2.py
+++ mktest2.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
-#import re
+import re
 import sys
+
+
+def find_word(word, document):
+    with open(document) as document_file:
+        all_line = []
+        for line in document_file:
+            all_line.append(line)
+    sentence = 0
+    word_punctuation = []
+    for i in range(len(all_line)):
+        line_list = re.findall('\w+|\.|\?|\!', all_line[i])
+        word_punctuation += line_list
+        for j in range(len(word_punctuation)):
+            print(word_punctuation[j], end=' ')
+    return 0
 
 
 if __name__ == '__main__':
@@ -9,4 +24,5 @@ if __name__ == '__main__':
     query = args[1]
     with open(query) as query_file:
         for word in query_file:
-            print(word)
+            word = word.strip("\n")
+            find_word(word, document)
