c00f5424b93b2e575bba407140bea9f34382ba14
lxxlccly <975716738@qq.com>
2020-02-28 10:55:53 +0800

    mktest2

--- mktest2.py
+++ mktest2.py
@@ -1,27 +1,36 @@
+'''从一个txt文档中找出某些单词所在的位置'''
 #!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+
 import re
 import sys
 
 
-def find_word(word, document):
-    with open(document) as document_file:
+def find_word(word1, document1):
+    '''找出一个单词在文档中的位置。
+
+    以问号、句号、感叹号为句子分隔符，输出第几句/第几个词。'''
+    with open(document1) as document_file:
         all_line = []
         for line in document_file:
             all_line.append(line)
     sentence = 0
     word_amount = 0
     word_punctuation = []
-    for i in range(len(all_line)):
-        line_list = re.findall('\w+|\.|\?|\!', all_line[i])
+    length_all_line = len(all_line)
+    for i in range(length_all_line):
+        line_list = re.findall(r'\w+|\.|\?|\!', all_line[i])
         word_punctuation += line_list
         while 1:
             has_punctuation = 0
-            for j in range(len(word_punctuation)):
+            length_word_punctuation = len(word_punctuation)
+            for j in range(length_word_punctuation):
                 if word_punctuation[j] in {'.', '!', '?'}:
                     has_punctuation = 1
                     sentence += 1
                     for k in range(0, j):
-                        if word_punctuation[k].lower() == word.lower():
+                        if word_punctuation[k].lower() == word1.lower():
                             word_amount += 1
                             if word_amount > 1:
                                 print(',{0}/{1}'.format(sentence, k+1), end='')
@@ -38,11 +47,11 @@ def find_word(word, document):
 
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    document = args[0]
-    query = args[1]
-    with open(query) as query_file:
+    ARGS = sys.argv[1:]
+    DOCUMENT = ARGS[0]
+    QUERY = ARGS[1]
+    with open(QUERY) as query_file:
         for word in query_file:
             word = word.strip("\n")
-            find_word(word, document)
+            find_word(word, DOCUMENT)
             print()
